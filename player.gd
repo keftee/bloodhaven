ed1d3ade9c8513e7cd62325c92893f3465f12cff
sohailkazi <sohailkazi001@gmail.com>
2024-01-26 18:28:05 +0530

    Revert "Sohaildev"

--- player.gd
+++ player.gd
@@ -1,13 +1,11 @@
 extends CharacterBody2D
 
+@onready var animation = $AnimatedSprite2D
 
-@onready var animation = $AnimationPlayer
-@onready var sprite = $Sprite2D
 
 @export var SPEED = 150.0
 @export var JUMP_VELOCITY = -300.0
 @export var DOUBLE_JUMP_VELOCITY = -200.0
-
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -32,13 +30,6 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("left", "right")
 	if direction:
-		if direction < -0.1:
-			sprite.set_flip_h(true)
-		if direction > 0.1:
-			sprite.set_flip_h(false)
-			
-
-		animation.play("walk")
 		velocity.x = direction * SPEED
 		if direction < -0.1:
 			animation.set_flip_h(true)
